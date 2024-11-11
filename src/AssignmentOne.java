0d6a83fc5c5d79ccc9c238dc02492eb0ac9bd7e5
Kobe-D <kobejdonovan@gmail.com>
2024-11-11 16:37:39 +1000

    Initial commit

---
+++ src/AssignmentOne.java
@@ -0,0 +1,25 @@
+public class AssignmentOne {
+    public static void main(String[] args) {
+
+    }
+    
+}
+
+class HealthProfessionals { 
+ // add my variables 
+    private int id;
+    private String name;
+    private String basicinfo;
+
+
+// default constructor 
+
+    public HealthProfessionals() {
+// initialize the variables
+        this.id = 0;
+        this.name = "";
+        this.basicinfo = "";
+
+    }   
+
+}
