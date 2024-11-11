741790a84e06b2b0aa8bdd74c81b2c05720a2896
Kobe-D <kobejdonovan@gmail.com>
2024-11-11 21:14:35 +1000

    fixed file naming
    miss reading and understanding of the task made me  try and do a 2 file job within the 1 file. two files have now been made to fix this issue

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -4,35 +4,4 @@ public class AssignmentOne {
     }
     
 }
- public class HealthProfessionals { 
- // add my variables 
-    private int id;
-    private String name;
-    private String basicinfo;
-
-
-// default constructor for variables 
-
-    public HealthProfessionals(int id, String name, String basicinfo) {
-// initialize the variables
-        this.id = 0;
-        this.name = "";
-        this.basicinfo = "";
-
-    }   
-
-}
-// implementing way to obtain client information
-
-public int getId() {
-    return id;
-
-}
-
-public String getName() {
-    return name;
-}
-
-public String getBasicinfo() {
-    return basicinfo;
-}
+ 
