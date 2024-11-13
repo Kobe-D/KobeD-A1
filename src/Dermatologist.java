42a8b9bd0334916b00f7813e643922ec0464c914
Kobe-D <kobejdonovan@gmail.com>
2024-11-13 12:25:38 +1000

    set up dermatologist.java

--- src/Dermatologist.java
+++ src/Dermatologist.java
@@ -1,1 +1,26 @@
 // Part - 2 child class(es) 
+
+public class Dermatologist extends HealthProfessional {
+    // stating its more then a GP and a specialised heal worker 
+    private String specialist;
+
+    //constructor 
+    public Dermatologist(int id, String name, String basicinfo, String specialist) {
+        // call the parent constructor from Healthprofesonal
+        super(id, name, basicinfo);
+        this.specialist = specialist;
+    }
+
+    //getter
+    public String getSpecialist() {
+        return specialist;
+    }
+
+    //setter 
+    public void SetSpecialist(String Specialsist) {
+        this.specialist = specialist;
+    }
+
+}
+
+// add a method to update print to include specialist 
--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -1,22 +1,22 @@
 // Part - 2 child class(es) 
 
 public class GeneralPractitioner extends HealthProfessional{
-    private  String GP; 
+    private  String gp; 
 
     // making child classess
     public GeneralPractitioner(int id, String name, String basicinfo) {
     super(id, name, basicinfo);
-    this.GP = GP;
+    this.gp = gp;
 
     }   
 
     //getter for GP
     public String getGP() {
-        return GP;
+        return gp;
     }
 // setter for GP
-    public void setGP(String GP) {
-        this.GP = GP;
+    public void setGP(String gp) {
+        this.gp = gp;
     }
 
 // adjust the print method to include GP
