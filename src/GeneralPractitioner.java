623bacba85ad12741e264e20159cc586dcbfa159
Kobe-D <kobejdonovan@gmail.com>
2024-11-13 14:16:06 +1000

    multiple changes
    cleaned up indentation
    fixed capitalization
    fixed error in print where a = was in place instead of a +

--- src/GeneralPractitioner.java
+++ src/GeneralPractitioner.java
@@ -4,24 +4,26 @@ public class GeneralPractitioner extends HealthProfessional{
     private  String gp; 
 
     // making child classess
-    public GeneralPractitioner(int id, String name, String basicinfo) {
+    public GeneralPractitioner(int id, String name, String basicinfo, String gp) {
     super(id, name, basicinfo);
     this.gp = gp;
 
     }   
 
     //getter for GP
-    public String getGP() {
-        return gp;
+public String getGP() {
+    return gp;
     }
 // setter for GP
-    public void setGP(String gp) {
-        this.gp = gp;
+public void setGP(String gp) {
+    this.gp = gp;
     }
 
 // adjust the print method to include GP
 
-
+public void printDetails() {    
+    System.out.print("gp = " + gp);
+}
 
 
 }
